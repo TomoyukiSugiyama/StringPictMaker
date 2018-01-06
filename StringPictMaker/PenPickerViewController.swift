28509061aca3cce1a11b4ce5749765d270f2cf90
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2018-01-06 14:13:18 +0900

    checkpoint.

--- StringPictMaker/PenPickerViewController.swift
+++ StringPictMaker/PenPickerViewController.swift
@@ -23,8 +23,23 @@ class PenPickerViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
 		print("PenPickerViewController - viewDidLoad")
-        // Do any additional setup after loading the view.
+		self.view.frame = CGRect(x:0,y:self.view.frame.height - 150 - 40,width:self.view.frame.width,height:150)
+		self.view.backgroundColor = UIColor.black
+		// スライダーの作成
+		let slider = UISlider()
+		slider.sizeToFit()
+		slider.center = self.view.center
+		
+		// スライダーの値が変更された時に呼び出されるメソッドを設定
+		slider.addTarget(self, action: #selector(onChange), for: .valueChanged)
+		
+		// スライダーを画面に追加
+		self.view.addSubview(slider)
     }
+	@objc func onChange(_ sender: UISlider) {
+		// スライダーの値が変更された時の処理
+		print(sender.value)
+	}
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
