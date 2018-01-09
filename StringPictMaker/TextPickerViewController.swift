b539754f71a26a32d4f3dc5a137150ead686e005
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2018-01-09 19:45:16 +0900

    Add new file. (TextPickerViewController.swift)

---
+++ StringPictMaker/TextPickerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TextPickerViewController.swift
+//  StringPictMaker
+//
+//  Created by 杉山智之 on 2018/01/09.
+//  Copyright © 2018年 杉山智之. All rights reserved.
+//
+
+import UIKit
+
+class TextPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
